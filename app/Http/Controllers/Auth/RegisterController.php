76683a12ae42af4bedfbb7c1c05f7f31a2d25149
mitokande <benmithat18@gmail.com>
2022-08-22 16:41:59 +0300

    shop_name typo

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -24,7 +24,7 @@ class RegisterController extends Controller
             "password" => $password,
             "username" => $username,
             "email" => $email,
-            "shopName" => $shopName
+            "shop_name" => $shopName
         ]);
 
         $decodedResponse = json_decode($response);
