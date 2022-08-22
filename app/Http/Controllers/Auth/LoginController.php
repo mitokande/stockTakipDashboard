77500db9b94b828bc684acf12e9f65d8089a19f3
Feanor <beratyesbek@gmail.com>
2022-08-22 12:42:09 +0300

    code fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,6 @@ class LoginController extends Controller
         $responseData = json_decode($response);
         if ($responseData->success)
         {
-            Session::put($username,$responseData->data->userToken);
             return $responseData->message;
         }
       return $responseData->message;
