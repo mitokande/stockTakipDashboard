c7ed9cb3fc33846c9b51b2bce4203d494454e47c
Feanor <beratyesbek@gmail.com>
2022-08-22 12:12:37 +0300

    Login controller and view

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+
+    public function login()
+    {
+    return view('Auth.login');
+    }
+}
